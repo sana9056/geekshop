ab387f62af6d8aba075a146ef67adb8531b1d7e6
sana9056 <sana9056@mail.ru>
2021-11-19 21:06:48 +0300

    Организовать вывод динамического контента на страницах index и products (список товаров, заголовок страницы). Поработать с шаблонными тегами и placeholder'ами (заглавные буквы, вывод текущей даты в шаблоне). Реализовать в проекте механизмы работы c URL-адресами. Реализовать наследование шаблонов в проекте.

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.contrib import admin
+from django.urls import path
+
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 
 
-def main(request):
-    return render(request, 'mainapp/index.html')
-    
-
 def products(request):
     return render(request, 'mainapp/products.html')
     
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+def main(request):
+    context = {
+        'title': 'GeekShop',
+        'header': 'Добро пожаловать на сайт',
+    }
+    return render(request, 'mainapp/index.html', context)
+
