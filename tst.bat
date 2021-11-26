f410d3e033cabcb92a95770f26a76eadcc49db45
sana9056 <sana9056@mail.ru>
2021-11-26 13:56:46 +0300

    Создать модель пользователя в проекте. Обязательно добавить поле с изображением и возраст . Выполнить настройки в файле конфигурации. Реализовать механизм аутентификации и авторизации в проекте. Реализовать механизм регистрации пользователя. И не забыть добавить logout Создать base.html для login.html и register.html в templates папке приложения authapp.

---
+++ tst.bat
@@ -0,0 +1,3 @@
+python -X utf8 manage.py dumpdata mainapp.ProductCategory -o mainapp\fixtures\categories.json
+python -X utf8 manage.py dumpdata mainapp.Product -o mainapp\fixtures\products.json
+pause
