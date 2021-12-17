0b10f088c675b88a2f9f521e1f54f2c0186c10b6
sana9056 <sana9056@mail.ru>
2021-12-17 08:36:13 +0300

    Домашняя №7

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     path('', mainapp.main, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
-    path('admin/', admin.site.urls),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
+    path('admin/', include('adminapp.urls', namespace='admin')),
 ]
 
 from django.conf import settings
