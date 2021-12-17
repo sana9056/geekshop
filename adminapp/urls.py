069bb117a9764c469abc0a58afedf1786a8a4f36
sana9056 <sana9056@mail.ru>
2021-12-17 09:09:52 +0300

    Домашняя №8

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('products/read/<int:pk>/', adminapp.product_read, name='product_read'),
     path('products/update/<int:pk>/', adminapp.product_update, name='product_update'),
     path('products/delete/<int:pk>/', adminapp.product_delete, name='product_delete'),
+    path('users/read/', adminapp.UsersListView.as_view(), name='users'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,6 +3,7 @@ from .models import ProductCategory, Product
 from django.shortcuts import get_object_or_404
 from basketapp.models import Basket
 import random
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def get_basket(user):
@@ -38,27 +39,37 @@ def main(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request, pk=None):
+def products(request, pk=None, page=1):
     title = 'продукты'
-    links_menu = ProductCategory.objects.all()
-    basket = get_basket(request.user)
+    links_menu = ProductCategory.objects.filter(is_active=True)
+    basket = getBasket(request.user)
 
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
-
-    if pk:
-        if pk == '0':
-            products = Product.objects.all().order_by('price')
-            category = {'name': 'все'}
+    if pk is not None:
+        if pk == 0:
+            category = {
+                'pk': 0,
+                'name': 'все'
+            }
+            products = Product.objects.filter(is_active=True, \
+                                              category__is_active=True).order_by('price')
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by('price')
+            products = Product.objects.filter(category__pk=pk, \
+                                              is_active=True, category__is_active=True).order_by('price')
+
+        paginator = Paginator(products, 2)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
 
         content = {
             'title': title,
             'links_menu': links_menu,
             'category': category,
-            'products': products,
+            'products': products_paginator,
             'basket': basket,
         }
 
